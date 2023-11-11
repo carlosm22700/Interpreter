d80787a714d6a5ea5d3b02723a4d8876e32be4aa
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-11-10 23:29:10 -0800

    complete parser to handle precedence and association

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -51,11 +51,13 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        //Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     // error() function and report() helper tell the user some syntax error occurred on a given line—this is bare minimum for error reporting.
@@ -66,6 +68,14 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {    
+        if (token.type == TokenType.EOF) {      
+            report(token.line, " at end", message);    
+        } else {      
+            report(token.line, " at '" + token.lexeme + "'", message);    
+        }  
     }
 }
 
