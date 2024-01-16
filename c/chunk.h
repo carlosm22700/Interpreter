af958f6fcac6f189dce04627497a4114eaa568d4
carlosm22700 <carlosmartinez.steven@gmail.com>
2024-01-16 02:22:59 -0800

    setting up foundation for bytecod

--- c/chunk.h
+++ c/chunk.h
@@ -3,19 +3,22 @@
 
 #include "common.h"
 
-typedef enum {
+typedef enum
+{
     OP_RETURN,
 } OpCode;
 
-typedef struct {
+typedef struct
+{
     // In addition to arr itself, we keep the number of elems in the array we have allocated (capacity) and how many of those entries are in use (count).
     int count;
     int capacity;
 
-    uint8_t* code;
+    uint8_t *code;
 } Chunk;
 
-void initChunk(Chunk* Chunk);
-void writeChunk(Chunk* Chunk, uint8_t byte);
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
 
 #endif
